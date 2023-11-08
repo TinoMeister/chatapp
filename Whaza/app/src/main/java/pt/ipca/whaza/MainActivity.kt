1ab0fb603d888162cd5396677864790f16e33c18
Francisco Arantes <Francisco.arantes.16864@gmail.com>
2023-11-08 12:38:38 +0000

    intent added

--- Whaza/app/src/main/java/pt/ipca/whaza/MainActivity.kt
+++ Whaza/app/src/main/java/pt/ipca/whaza/MainActivity.kt
@@ -1,9 +1,11 @@
 package pt.ipca.whaza
 
+import android.app.ActivityOptions
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.transition.ChangeBounds
+import android.util.Pair
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
@@ -21,6 +23,7 @@ class MainActivity : AppCompatActivity() {
     val password: TextInputLayout by lazy {
         findViewById(R.id.login_password_til)
     }
+    val db = Firebase.firestore
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,6 +37,21 @@ class MainActivity : AppCompatActivity() {
 
         val name = findViewById<TextView>(R.id.login_welcome_tv)
         val desc = findViewById<TextView>(R.id.login_desc_tv)
+
+        val btnSignUp = findViewById<Button>(R.id.login_register_btn)
+        btnSignUp.setOnClickListener {
+            val intent = Intent(this@MainActivity, RegisterActivity::class.java)
+
+            val options = ActivityOptions.makeSceneTransitionAnimation(this@MainActivity,
+                Pair.create(name, "name_tn"),
+                Pair.create(desc, "desc_tn"),
+                Pair.create(email, "email_tn"),
+                Pair.create(password, "password_tn"),
+                Pair.create(btn, "signin_tn"),
+                Pair.create(btnSignUp, "signin_signup_tn"))
+
+            startActivity(intent, options.toBundle())
+        }
     }
 
     fun doLogin(v: View) {
@@ -57,4 +75,6 @@ class MainActivity : AppCompatActivity() {
                 }
             }
     }
+
+
 }
